b6339b9e98cffa13d85b3334221c2f087ca71c8e
Anand <anand@sparkouttech.com>
2023-08-24 14:52:20 +0530

    Initial Commit

---
+++ AnimationDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimationDemo
+//
+//  Created by Sparkout on 24/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
